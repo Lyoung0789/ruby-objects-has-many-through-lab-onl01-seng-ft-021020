8e2b24440d3ae9c91e1597518aeb3c343405d8a1
Lester Young <lyoung070789@gmail.com>
2020-03-10 03:25:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,14 +14,10 @@ class Patient
   
   def new_appointment(doctor, date)
    appointment = Appointment.new(self, date, doctor)
-   appointment 
-   
   end
  
   
-  def appointments
-    # Appointment.all.select{|apppointments| appointments.patient == self}
-  end 
+  
 
 
 
