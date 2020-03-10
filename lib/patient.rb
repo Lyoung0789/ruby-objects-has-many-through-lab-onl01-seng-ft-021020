f67aeb2961e20e9c6746db0beeede550980d5139
Lester Young <lyoung070789@gmail.com>
2020-03-10 03:47:59 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,7 @@ class Patient
  end 
  
   def appointments 
-    Appointment.all.select{|appointments| binding.pry appointments.patient == self}
+    Appointment.all.select{|appointments| appointments.patient == self}
   end 
   
   def doctors 
